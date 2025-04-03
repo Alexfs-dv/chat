c25278a2eb2c9fe023c7f72ff8549198e245af70
Alex <alexfaustinosouza@gmail.com>
2025-04-03 19:45:56 -0400

    atualizacao

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
 const express = require("express");
-const http = require("http");
+const http = require("https://chat-psi-seven-14.vercel.app/p");
 const { Server } = require("socket.io");
-const cors = require("cors"); // Importar o pacote cors
+const cors = require("cors");
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-app.use(cors()); // Configurar CORS
-app.use(express.static(__dirname)); // Serve arquivos estáticos
+app.use(cors());
+app.use(express.static(__dirname));
 
 io.on("connection", (socket) => {
-  console.log("Um usuário conectado");
+  console.log("Um usuário conectado: " + socket.id); // Log de conexão
 
   socket.on("sendMessage", (message) => {
-    io.emit("receiveMessage", message); // Envia a mensagem para todos os usuários conectados
+    io.emit("receiveMessage", message);
   });
 
   socket.on("disconnect", () => {
-    console.log("Um usuário desconectado");
+    console.log("Um usuário desconectado: " + socket.id); // Log de desconexão
   });
 });
 
